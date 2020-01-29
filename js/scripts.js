d3904eeb941823cd6480c4058eeb16a4ef3e7fb6
Dana Luukko <dcluukko@gmail.com>
2020-01-28 22:53:14 -0800

    created logic for prototypes containing .AddDeposit, .subWithdraw, .AddAccountInfo

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,54 @@
 // Business Logic for Account---------
 function BankAccount() {
-  this.Accountinfo = [];
+  this.AccountInfo = [];
   this.currentId = 0
 }
 
 //Name and Initial Deposit Business Logic 
-function Accountinfo(name, initDeposit) {
+function AccountInfo(name, totalBalance, deposit) {
   this.name = name,
-  this.initDeposit = initDeposit,
-  this.deposit = deposit,
-  this.withdraw = withdraw 
+    this.totalBalance = totalBalance;
+}
+
+AccountInfo.prototype.AddDeposit = function (deposit) {
+  this.totalBalance += deposit
+}
+AccountInfo.prototype.SubWithdraw = function (withdraw) {
+  this.totalBalance -= withdraw
 }
 
 //Deposit Business Logic 
-BankAccount.prototype.AddAccountInfo = function (AccountInfo)
-  
+BankAccount.prototype.AddAccountInfo = function (AccountInfo) {
+  this.AccountInfo.push(AccountInfo)
+}
+
 
 //User Interface Logic
 
 $(document).ready(function () {
-  event.preventDefault();
-  var name = $("input#name").val();
-  var InitDeposit = parseInt($("input#initialDeposit").val());
-  var Deposit = parseInt($("input#deposit").val());
-  var withdraw = parseInt($("input#withdraw").val());
-  var addDepositToInitDep = InitDeposit + Deposit
-  var addDepositToInitDep = new Account 
+  var userBankAccount = new BankAccount
+  var userAccountInfo;
+  $("form#name-initDept").submit(function (event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+    var InitDeposit = parseInt($("input#initialDeposit").val());
+    userAccountInfo = new AccountInfo(name, InitDeposit);
+    userBankAccount.AddAccountInfo(userAccountInfo);
+    console.log(userBankAccount);
+  });
+  $("form#deposit-withdrawl").submit(function (event) {
+    event.preventDefault();
+    console.log("hello!")
+    var deposit = parseInt($("input#deposit").val());
+    var withdraw = parseInt($("input#withdraw").val());
+    if (userAccountInfo) {
+      if (deposit) {
+        userAccountInfo.AddDeposit(deposit);
+      }
+      if (withdraw) {
+        userAccountInfo.SubWithdraw(withdraw);
+      }
+    }
+    console.log(userAccountInfo)
+  });
 });
